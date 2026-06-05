e697d1e7f8c5a7736a2e609f9eef1a364ad5e8d3
lucas <lucaswoo583@gmail.com>
2026-06-05 03:53:05 -0600

    NewAuthServiceClient

---
+++ internal/grpc/auth/client.go
@@ -0,0 +1,30 @@
+package authgrpc
+
+import (
+	"log"
+	"os"
+
+	authv1 "github.com/lucas-woo/cloud-drive/api/auth/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+
+func NewAuthServiceClient() authv1.AuthServiceClient {
+	
+	port, found := os.LookupEnv("AUTH_GRPC_PORT")
+
+	if !found {
+		log.Fatal("error with auth port env");
+	}
+
+	conn, err := grpc.NewClient("localhost:" + port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatal()
+	}
+
+	client := authv1.NewAuthServiceClient(conn)
+
+	return client
+}
