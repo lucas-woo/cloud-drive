0afe89870d8467595e694da6ccc3e82fbbcbb193
lucas <lucaswoo583@gmail.com>
2026-06-06 02:39:35 -0600

    validating user session rpc method

--- api/auth/v1/service_grpc.pb.go
+++ api/auth/v1/service_grpc.pb.go
@@ -19,9 +19,10 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	AuthService_SignUpUser_FullMethodName = "/auth.v1.AuthService/SignUpUser"
-	AuthService_LoginUser_FullMethodName  = "/auth.v1.AuthService/LoginUser"
-	AuthService_LogoutUser_FullMethodName = "/auth.v1.AuthService/LogoutUser"
+	AuthService_SignUpUser_FullMethodName          = "/auth.v1.AuthService/SignUpUser"
+	AuthService_LoginUser_FullMethodName           = "/auth.v1.AuthService/LoginUser"
+	AuthService_LogoutUser_FullMethodName          = "/auth.v1.AuthService/LogoutUser"
+	AuthService_ValidateUserSession_FullMethodName = "/auth.v1.AuthService/ValidateUserSession"
 )
 
 // AuthServiceClient is the client API for AuthService service.
@@ -31,6 +32,7 @@ type AuthServiceClient interface {
 	SignUpUser(ctx context.Context, in *SignUpUserRequest, opts ...grpc.CallOption) (*SignUpUserResponse, error)
 	LoginUser(ctx context.Context, in *LoginUserRequest, opts ...grpc.CallOption) (*LoginUserResponse, error)
 	LogoutUser(ctx context.Context, in *LogoutUserRequest, opts ...grpc.CallOption) (*LogoutUserResponse, error)
+	ValidateUserSession(ctx context.Context, in *ValidateUserSessionRequest, opts ...grpc.CallOption) (*ValidateUserSessionResponse, error)
 }
 
 type authServiceClient struct {
@@ -71,6 +73,16 @@ func (c *authServiceClient) LogoutUser(ctx context.Context, in *LogoutUserReques
 	return out, nil
 }
 
+func (c *authServiceClient) ValidateUserSession(ctx context.Context, in *ValidateUserSessionRequest, opts ...grpc.CallOption) (*ValidateUserSessionResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(ValidateUserSessionResponse)
+	err := c.cc.Invoke(ctx, AuthService_ValidateUserSession_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // AuthServiceServer is the server API for AuthService service.
 // All implementations must embed UnimplementedAuthServiceServer
 // for forward compatibility.
@@ -78,6 +90,7 @@ type AuthServiceServer interface {
 	SignUpUser(context.Context, *SignUpUserRequest) (*SignUpUserResponse, error)
 	LoginUser(context.Context, *LoginUserRequest) (*LoginUserResponse, error)
 	LogoutUser(context.Context, *LogoutUserRequest) (*LogoutUserResponse, error)
+	ValidateUserSession(context.Context, *ValidateUserSessionRequest) (*ValidateUserSessionResponse, error)
 	mustEmbedUnimplementedAuthServiceServer()
 }
 
@@ -96,6 +109,9 @@ func (UnimplementedAuthServiceServer) LoginUser(context.Context, *LoginUserReque
 }
 func (UnimplementedAuthServiceServer) LogoutUser(context.Context, *LogoutUserRequest) (*LogoutUserResponse, error) {
 	return nil, status.Error(codes.Unimplemented, "method LogoutUser not implemented")
+}
+func (UnimplementedAuthServiceServer) ValidateUserSession(context.Context, *ValidateUserSessionRequest) (*ValidateUserSessionResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "method ValidateUserSession not implemented")
 }
 func (UnimplementedAuthServiceServer) mustEmbedUnimplementedAuthServiceServer() {}
 func (UnimplementedAuthServiceServer) testEmbeddedByValue()                     {}
@@ -172,6 +188,24 @@ func _AuthService_LogoutUser_Handler(srv interface{}, ctx context.Context, dec f
 	return interceptor(ctx, in, info, handler)
 }
 
+func _AuthService_ValidateUserSession_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ValidateUserSessionRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AuthServiceServer).ValidateUserSession(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: AuthService_ValidateUserSession_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AuthServiceServer).ValidateUserSession(ctx, req.(*ValidateUserSessionRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // AuthService_ServiceDesc is the grpc.ServiceDesc for AuthService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -190,6 +224,10 @@ var AuthService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "LogoutUser",
 			Handler:    _AuthService_LogoutUser_Handler,
+		},
+		{
+			MethodName: "ValidateUserSession",
+			Handler:    _AuthService_ValidateUserSession_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
