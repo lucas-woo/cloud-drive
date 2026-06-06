062df5951b8664c34190ee2d0f43ed6f4c0d0630
lucas <lucaswoo583@gmail.com>
2026-06-06 03:19:37 -0600

    config file

---
+++ internal/repository/auth_repository.go
@@ -0,0 +1,17 @@
+package repository
+
+import (
+	"github.com/lucas-woo/cloud-drive/internal/config"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+type AuthRepo struct {
+	db *mongo.Collection
+	
+}
+
+func NewAuthRepo(mongoClient *mongo.Client) *AuthRepo {
+	return &AuthRepo{
+		db: mongoClient.Database(config.AuthDatabaseName).Collection(config.AuthCollection),
+	}
+}
