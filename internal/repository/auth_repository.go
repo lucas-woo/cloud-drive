bb9ef6e7d9143305a689f68d5f3e665a53add43f
lucas <241841693+lucas-woo@users.noreply.github.com>
2026-06-07 05:31:16 -0600

    need to create redis repository

--- internal/repository/auth_repository.go
+++ internal/repository/auth_repository.go
@@ -3,10 +3,13 @@ package repository
 import (
 	"context"
 
+	"github.com/google/uuid"
 	"github.com/lucas-woo/cloud-drive/internal/config"
+	"github.com/lucas-woo/cloud-drive/internal/models"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthRepo struct {
@@ -27,6 +30,30 @@ func (r *AuthRepo) EmailTaken(ctx context.Context, email string) (bool, error) {
 		return true, nil
 	}
 	return false, nil
+}
+
+func (r *AuthRepo) CreateNewUser(ctx context.Context, user models.SignUpUserRequest) (string, error) {
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	newUserId := uuid.New()
+
+	newUser := &models.UserModel{
+		Hash: string(hash),
+		Email: user.Email,
+		UserID: newUserId,
+	}
+
+	_, err = r.db.InsertOne(ctx, newUser)
+
+	if err != nil {
+		return "", err
+	}
+
+	return newUserId.String(), nil
 }
 
 func NewAuthRepo(mongoClient *mongo.Client) *AuthRepo {
