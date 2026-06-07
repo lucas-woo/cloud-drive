97f0e7ff41d14d74693e35c45f72088188dffb67
lucas <lucaswoo583@gmail.com>
2026-06-07 03:45:43 -0600

    auth resources in new server

--- internal/grpc/auth/auth.go
+++ internal/grpc/auth/auth.go
@@ -4,15 +4,18 @@ import (
 	"context"
 
 	authv1 "github.com/lucas-woo/cloud-drive/api/auth/v1"
+	"github.com/lucas-woo/cloud-drive/internal/database"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 type Server struct {
 	authv1.UnimplementedAuthServiceServer
+	authResources *database.AuthResources
 }
 
 func (s *Server) SignUpUser(context.Context, *authv1.SignUpUserRequest) (*authv1.SignUpUserResponse, error) {
+	
 	return nil, status.Error(codes.Unimplemented, "method SignUpUser not implemented")
 }
 func (Server) LoginUser(context.Context, *authv1.LoginUserRequest) (*authv1.LoginUserResponse, error) {
@@ -20,4 +23,10 @@ func (Server) LoginUser(context.Context, *authv1.LoginUserRequest) (*authv1.Logi
 }
 func (Server) LogoutUser(context.Context, *authv1.LogoutUserRequest) (*authv1.LogoutUserResponse, error) {
 	return nil, status.Error(codes.Unimplemented, "method LogoutUser not implemented")
+}
+
+func NewAuthServer(authResources *database.AuthResources) *Server {
+	return &Server{
+		authResources: authResources,
+	}
 }
