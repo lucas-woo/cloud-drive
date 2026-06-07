26310290ea5eb1340928c1b9cebd64647b762ba7
lucas <241841693+lucas-woo@users.noreply.github.com>
2026-06-07 05:05:51 -0600

    register

--- internal/grpc/auth/auth.go
+++ internal/grpc/auth/auth.go
@@ -5,6 +5,7 @@ import (
 
 	authv1 "github.com/lucas-woo/cloud-drive/api/auth/v1"
 	"github.com/lucas-woo/cloud-drive/internal/database"
+	"github.com/lucas-woo/cloud-drive/internal/models"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -15,8 +16,14 @@ type Server struct {
 }
 
 func (s *Server) SignUpUser(ctx context.Context, req *authv1.SignUpUserRequest) (*authv1.SignUpUserResponse, error) {
-
-	sessionId, err := s.service.Register()
+	
+	sessionId, err := s.service.Register(ctx, models.SignUpUserRequest{
+		Username: req.GetUsername(),
+		Password: req.GetPassword(),
+		Email: req.GetEmail(),
+		RememberMe: req.GetRememberMe(),
+	})
+	
 	if err != nil {
 		return nil, status.Error(codes.Internal, err.Error())
 	}
--- internal/grpc/auth/service.go
+++ internal/grpc/auth/service.go
@@ -1,13 +1,24 @@
 package authgrpc
 
-import "github.com/lucas-woo/cloud-drive/internal/database"
+import (
+	"context"
+
+	"github.com/lucas-woo/cloud-drive/internal/database"
+	"github.com/lucas-woo/cloud-drive/internal/models"
+	"golang.org/x/crypto/bcrypt"
+)
 
 type Service struct {	
 	authResources *database.AuthResources
 }
 
 // returns session id, err
-func (s *Service) Register() (string, error) {
+func (s *Service) Register(ctx context.Context, user models.SignUpUserRequest) (string, error) {
+	
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
 	return "", nil
 }
 
