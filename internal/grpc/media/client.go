8fe9f91410056e77c27c575d023e3430722ef84c
lucas <241841693+lucas-woo@users.noreply.github.com>
2026-06-11 06:48:39 -0600

    media service client

---
+++ internal/grpc/media/client.go
@@ -0,0 +1,30 @@
+package media
+
+import (
+	"log"
+	"os"
+
+	mediav1 "github.com/lucas-woo/cloud-drive/api/media/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+
+func NewMediaServiceClient() mediav1.MediaServiceClient {
+	
+	port, found := os.LookupEnv("MEDIA_CLIENT_PORT")
+
+	if !found {
+		log.Fatal("error with media port env");
+	}
+
+	conn, err := grpc.NewClient("localhost:" + port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatal()
+	}
+
+	client := mediav1.NewMediaServiceClient(conn)
+
+	return client
+}
