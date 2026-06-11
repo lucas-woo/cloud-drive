85beff53f4e0d19c1664f32f7b015db83b61f090
lucas <241841693+lucas-woo@users.noreply.github.com>
2026-06-11 06:51:54 -0600

    implementing media grpc server

--- internal/grpc/media/media.go
+++ internal/grpc/media/media.go
@@ -1,1 +1,28 @@
 package media
+
+import (
+	"context"
+
+	mediav1 "github.com/lucas-woo/cloud-drive/api/media/v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type Server struct {
+	mediav1.UnimplementedMediaServiceServer
+	service *Service
+}
+
+func (s *Server) CreateNewProject(context.Context, *mediav1.CreateNewProjectRequest) (*mediav1.CreateNewProjectResponse, error) {
+
+	
+	return nil, status.Error(codes.Unimplemented, "method CreateNewProject not implemented")
+
+
+}
+
+func NewMediaServer(mediaResources ) *Server {
+	return &Server{
+
+	}
+}
