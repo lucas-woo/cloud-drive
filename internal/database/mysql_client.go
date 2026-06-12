734098af40edf85a715bbc9c9bb95eb559809a75
lucas <241841693+lucas-woo@users.noreply.github.com>
2026-06-12 07:09:11 -0600

    connecting to mysql

---
+++ internal/database/mysql_client.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func ConnectMySql() (*sql.DB) {
+
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+		user, pass, host, port, dbName,
+	)	
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal("failed to open db:", err)
+	}	
+
+	fmt.Println("connected to mysql")
+	return db
+}
