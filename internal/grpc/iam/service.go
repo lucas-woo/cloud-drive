bacc5630addf4384b5b8a6550b06bab639d2f403
lucas <lucaswoo583@gmail.com>
2026-06-13 06:17:52 -0600

    resources

--- internal/grpc/iam/service.go
+++ internal/grpc/iam/service.go
@@ -1,9 +1,11 @@
 package iamgrpc
 
+import "github.com/lucas-woo/cloud-drive/internal/database"
+
 type Service struct {
 
 }
 
-func NewIamService() *Service {
+func NewIamService(iamResources *database.IamResources) *Service {
 	return &Service{}
 }
