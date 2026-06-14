8351f51e4dc60e229d312c23d5f68eb4e61a4e41
lucas <241841693+lucas-woo@users.noreply.github.com>
2026-06-14 08:42:08 -0600

    validating permission proto

--- internal/grpc/iam/iam.go
+++ internal/grpc/iam/iam.go
@@ -8,6 +8,7 @@ import (
 	"github.com/lucas-woo/cloud-drive/internal/dto"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 
@@ -18,15 +19,23 @@ type Server struct {
 
 func (s *Server) GenerateNewApiKey(ctx context.Context, req *iamv1.GenerateNewApiKeyRequest) (*iamv1.GenerateNewApiKeyResponse, error) {
 
-	s.service.GenerateNewApiKey(ctx, &dto.GenerateNewApiKeyRequest{
+	res, err := s.service.GenerateNewApiKey(ctx, &dto.GenerateNewApiKeyRequest{
 		SessionId: req.GetSessionId(),
 		KeyName: req.GetKeyName(),
 		ProjectId: req.GetProjectId(),
 	})
-
-	return nil, status.Error(codes.Unimplemented, "method GenerateNewApiKey not implemented")
+	if err != nil {
+		return nil, status.Error(codes.PermissionDenied, err.Error())
+	}
+	return &iamv1.GenerateNewApiKeyResponse{
+		ApiKey: res.ApiKey,
+		ApiSecret: res.ApiSecret,
+		CreatedAt: timestamppb.New(res.CreatedAt),
+	}, nil
 }
-func (s *Server) ValidateApiKey(ctx context.Context, req *iamv1.ValidateApiKeyRequest) (*iamv1.ValidateApiKeyResponse, error) {
+
+
+func (s *Server) ValidateApiKeyPermission(ctx context.Context, req *iamv1.ValidateApiKeyRequest) (*iamv1.ValidateApiKeyResponse, error) {
 	return nil, status.Error(codes.Unimplemented, "method ValidateApiKey not implemented")
 }
 
