962f785b13614354cb91dc7a16dcb6c0640fcc53
lucas <lucaswoo583@gmail.com>
2026-06-14 07:50:25 -0600

    generate api secret

--- internal/repository/apikeys/apikeys_repository.go
+++ internal/repository/apikeys/apikeys_repository.go
@@ -3,17 +3,17 @@ package apikeysrepository
 import (
 	"context"
 	"database/sql"
+
+	"github.com/lucas-woo/cloud-drive/internal/dto"
 )
 
 type ApiKeysRepository struct {
 	mysql *sql.DB
 }
 
-
-
-func (r *ApiKeysRepository) GenerateApiKeyAndSecret(ctx context.Context) (error) {
-
-
+func (r *ApiKeysRepository) GenerateApiKeyAndSecret(ctx context.Context, req *dto.CreateNewProjectRequest) (error) {
+	
+	return nil
 }
 
 func NewApiKeysRepository(mySqlClient *sql.DB) *ApiKeysRepository {
--- internal/utils/api_keys.go
+++ internal/utils/api_keys.go
@@ -1,9 +1,25 @@
 package utils
 
-func GenerateApiKey() {
-	
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
+
+func GenerateApiKey() (string, error) {
+	temp := make([]byte, 16)
+
+	if _, err := rand.Read(temp); err != nil {
+		return "", nil;
+	}
+	return base64.URLEncoding.EncodeToString(temp), nil	
 }
 
-func GenerateApiSecret() {
+func GenerateApiSecret() (string, error) {
+	temp := make([]byte, 16)
 
+	if _, err := rand.Read(temp); err != nil {
+		return "", nil;
+	}
+	return base64.URLEncoding.EncodeToString(temp), nil
 }
+
