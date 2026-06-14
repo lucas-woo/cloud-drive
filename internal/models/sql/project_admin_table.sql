e11d9a456ea468e5c7d5d23576ef60f3c4185cac
lucas <lucaswoo583@gmail.com>
2026-06-14 06:12:23 -0600

    updated repo for roles

--- internal/models/sql/project_admin_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE project_admin (
-    
-    user_id BINARY(16) NOT NULL,
-    project_id BINARY(16) NOT NULL,
-
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
-    PRIMARY KEY (user_id, project_id)
-);
