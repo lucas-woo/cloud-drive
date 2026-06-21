275f7e01702b92776dc7e1aec9142e4f04eef6ed
lucas <241841693+lucas-woo@users.noreply.github.com>
2026-06-21 04:43:48 -0600

    media and iam server

---
+++ cmd/grpc/iam/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	iamv1 "github.com/lucas-woo/cloud-drive/api/iam/v1"
+	"github.com/lucas-woo/cloud-drive/internal/config"
+	"github.com/lucas-woo/cloud-drive/internal/database"
+	iamgrpc "github.com/lucas-woo/cloud-drive/internal/grpc/iam"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	healthv1 "google.golang.org/grpc/health/grpc_health_v1"
+)
+
+func main() {
+
+	err := config.InitializeEnv();
+	if err != nil {
+		log.Fatal(err)
+	}
+	config.InitCookiesEnv()
+
+	iamResources := database.NewIamResources()
+
+	port, found := os.LookupEnv("IAM_SERVER_PORT")
+
+	if !found {
+		log.Fatal("error with auth port env");
+	}	
+
+	lis, err := net.Listen("tcp", ":" + port)
+
+	if err != nil {
+		log.Fatalf("err %v", err);
+	}
+
+	grpcServer := grpc.NewServer();
+
+	iamv1.RegisterIAMServiceServer(grpcServer, iamgrpc.NewIamServer(iamResources))
+
+
+	healthServer := health.NewServer()
+	healthv1.RegisterHealthServer(grpcServer, healthServer);
+	fmt.Println("media server running")
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("err in starting grpc server: %v",err)
+	}	
+}
---
+++ cmd/grpc/media/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	mediav1 "github.com/lucas-woo/cloud-drive/api/media/v1"
+	"github.com/lucas-woo/cloud-drive/internal/config"
+	"github.com/lucas-woo/cloud-drive/internal/database"
+	mediagrpc "github.com/lucas-woo/cloud-drive/internal/grpc/media"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	healthv1 "google.golang.org/grpc/health/grpc_health_v1"
+)
+
+func main() {
+
+	err := config.InitializeEnv();
+	if err != nil {
+		log.Fatal(err)
+	}
+	config.InitCookiesEnv()
+
+	mediaResources := database.NewMediaResources()
+
+	port, found := os.LookupEnv("MEDIA_SERVER_PORT")
+
+	if !found {
+		log.Fatal("error with auth port env");
+	}	
+
+	lis, err := net.Listen("tcp", ":" + port)
+
+	if err != nil {
+		log.Fatalf("err %v", err);
+	}
+
+	grpcServer := grpc.NewServer();
+
+	mediav1.RegisterMediaServiceServer(grpcServer, mediagrpc.NewMediaServer(mediaResources))
+
+	healthServer := health.NewServer()
+	healthv1.RegisterHealthServer(grpcServer, healthServer);
+	fmt.Println("media server running")
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("err in starting grpc server: %v",err)
+	}	
+}
+
