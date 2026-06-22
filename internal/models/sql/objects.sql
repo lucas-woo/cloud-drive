b223a5c294692ffffffc1c7c5f5515456ac447e1
lucas <lucaswoo583@gmail.com>
2026-06-22 07:01:13 -0600

    uploading media proto

--- internal/models/sql/objects.sql
+++ internal/models/sql/objects.sql
@@ -5,9 +5,10 @@ CREATE TABLE project_objects (
     folder VARCHAR(255) NOT NULL,
 
     is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    is_pending BOOLEAN NOT NULL DEFAULT TRUE,
 
-    created_at TIMESTAMP NOT NULL,
-    modified_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NULL,
+    modified_at TIMESTAMP NULL,
 
     PRIMARY KEY (project_id, object_id)
 );
