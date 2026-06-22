d5610efdb155f41e0ec4fd7c45dad65786b67501
lucas <lucaswoo583@gmail.com>
2026-06-22 07:41:01 -0600

    s3 repository + resources init

---
+++ internal/repository/s3/s3_repository.go
@@ -0,0 +1,19 @@
+package s3repository
+
+import (
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+type S3Repository struct {
+	S3Client *s3.Client
+	PresignClient *s3.PresignClient
+	BucketName string
+}
+
+func NewS3Repository(	s3Client *s3.Client, presignClient *s3.PresignClient, bucketName string) *S3Repository {
+	return &S3Repository{
+		S3Client: s3Client,
+		PresignClient: presignClient,
+		BucketName: bucketName,
+	}
+}
