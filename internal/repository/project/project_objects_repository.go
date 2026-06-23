6962323233a993582e20e9e2e83d0a57ba1bc35d
lucas <lucaswoo583@gmail.com>
2026-06-23 04:39:37 -0600

    reorganizing files

---
+++ internal/repository/project/project_objects_repository.go
@@ -0,0 +1,7 @@
+package projectrepository
+
+import "context"
+
+func (r *ProjectRepository) CreateNewObject(ctx context.Context) error {
+	return nil
+}
--- internal/repository/project/project_repository.go
+++ internal/repository/project/project_repository.go
@@ -3,7 +3,6 @@ package projectrepository
 import (
 	"context"
 	"database/sql"
-	"fmt"
 	"time"
 
 	"github.com/google/uuid"
@@ -46,51 +45,6 @@ func (r *ProjectRepository) CreateNewProject(ctx context.Context, userId uuid.UU
 	err = r.AddProjectUserRole(ctx, userId, pId, config.ADMIN_ROLE)
 
 	return
-}
-
-
-func (r *ProjectRepository) AddProjectUserRole(ctx context.Context,userId uuid.UUID, projectId uuid.UUID, role string) error {
-	query := fmt.Sprintf(`
-		INSERT INTO %s (user_id, project_id, role)
-		VALUES (?, ?, ?)
-	`, config.ProjectUserRolesTable)
-
-	_, err := r.sqldb.ExecContext(ctx ,query, userId[:], projectId[:], role)
-	return err
-}
-
-func (r *ProjectRepository) CheckProjectUserRole(ctx context.Context, userId uuid.UUID, projectId uuid.UUID, role string) (bool, error) {
-	query := fmt.Sprintf(`
-		SELECT EXISTS(
-			SELECT 1
-			FROM %s
-			WHERE user_id = ?
-			AND project_id = ?
-			AND role = ?
-		)
-	`, config.ProjectUserRolesTable)
-
-	var exists bool
-
-	err := r.sqldb.QueryRowContext(ctx, query,userId[:],projectId[:],role,).Scan(&exists)
-
-	if err != nil {
-		return false, err
-	}
-
-	return exists, nil
-}
-
-func (r *ProjectRepository) DeleteProjectUserRole(ctx context.Context, userId uuid.UUID, projectId uuid.UUID, role string) error {
-	query := fmt.Sprintf(`
-		DELETE FROM %s
-		WHERE user_id = ?
-		  AND project_id = ?
-		  AND role = ?
-	`, config.ProjectUserRolesTable)
-
-	_, err := r.sqldb.ExecContext(ctx, query, userId[:], projectId[:], role)
-	return err
 }
 
 func NewProjectRepository(mongoClient *mongo.Client, mysqlClient *sql.DB) *ProjectRepository {
