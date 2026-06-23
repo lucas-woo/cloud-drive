fb556db548843e52da768e25525d7e042b8a6004
lucas <241841693+lucas-woo@users.noreply.github.com>
2026-06-23 04:26:01 -0600

    validating user session

--- internal/grpc/media/media.go
+++ internal/grpc/media/media.go
@@ -38,7 +38,10 @@ func (s *Server) CreateNewProject(ctx context.Context, req *mediav1.CreateNewPro
 
 func (s *Server) UploadObject(ctx context.Context, req *mediav1.UploadObjectRequest) (*mediav1.UploadObjectResponse, error) {
 	s.service.GetUploadObjectSignedUrl(ctx, &dto.UploadObjectRequest{
-		
+		SessionId: req.GetSessionId(),
+		ProjectId: req.GetProjectId(),
+		ObjectName: req.GetObjectName(),
+		Folder: req.GetFolder(),
 	})
 	return nil, status.Error(codes.Unimplemented, "method UploadObject not implemented")
 }
--- internal/grpc/media/service.go
+++ internal/grpc/media/service.go
@@ -2,9 +2,11 @@ package mediagrpc
 
 import (
 	"context"
+	"errors"
 
 	"github.com/google/uuid"
 	authv1 "github.com/lucas-woo/cloud-drive/api/auth/v1"
+	"github.com/lucas-woo/cloud-drive/internal/config"
 	"github.com/lucas-woo/cloud-drive/internal/database"
 	"github.com/lucas-woo/cloud-drive/internal/dto"
 )
@@ -35,6 +37,31 @@ func (s *Service) CreateNewProject(ctx context.Context, createNewProjectRequest 
 }
 
 func (s *Service) GetUploadObjectSignedUrl(ctx context.Context, req *dto.UploadObjectRequest) (url string, err error) {
+	res, err := s.mediaResources.AuthClient.ValidateUserSession(ctx, &authv1.ValidateUserSessionRequest{
+		SessionId: req.SessionId,
+	})
+	if err != nil{
+		return
+	}
+	userId, err := uuid.Parse(res.GetUserId())
+	if err != nil{
+		return 
+	}
+	projectId, err := uuid.Parse(res.GetUserId())
+	if err != nil{
+		return 
+	}
+	ok, err := s.mediaResources.ProjectRepository.CheckProjectUserRole(ctx, userId, projectId, config.ADMIN_ROLE)
+	if err != nil{
+		return 
+	}
+	if !ok {
+		return "", errors.New("doesn't have role")
+	}
+
+	
+
+
 	return "", nil
 }
 
