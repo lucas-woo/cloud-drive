0b28db981a090fe4aa041b90e87b66c7ed5945a0
lucas <lucaswoo583@gmail.com>
2026-07-17 17:39:47 -0600

    selecting image processor

---
+++ internal/utils/transformation.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os/exec"
+
+	mediav1 "github.com/lucas-woo/cloud-drive/api/media/v1"
+)
+
+//todo need to do all transformations
+func SelectImageProcessor(ctx context.Context, transformation *mediav1.ImageTransformations) (*exec.Cmd, error) {
+	
+	if transformation.GetScale() == nil {
+		return nil, errors.New("no Scale")
+	}
+
+	cmd := exec.CommandContext(ctx, "./image-processor", fmt.Sprintf("%d", transformation.Scale.GetWidth()), fmt.Sprintf("%d", transformation.Scale.GetHeight()))
+
+	return cmd, nil
+}
