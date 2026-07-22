93d81349fec9ac147a78ed21bb6f33d54db7b322
lucas <241841693+lucas-woo@users.noreply.github.com>
2026-07-21 20:08:47 -0600

    transformation util

--- internal/utils/transformation.go
+++ internal/utils/transformation.go
@@ -12,12 +12,23 @@ import (
 //todo need to do all transformations
 func SelectImageProcessor(ctx context.Context, transformation *mediav1.ImageTransformations) (*exec.Cmd, error) {
 	
-	if transformation.GetScale() == nil {
-		return nil, errors.New("no Scale")
+	if transformation.GetScale() != nil {
+		// need to add some Validation function for params
+		return exec.CommandContext(ctx, "bin/image-processor", "scale", fmt.Sprintf("%d", transformation.Scale.GetWidth()), fmt.Sprintf("%d", transformation.Scale.GetHeight())), nil
 	}
 
-	//cmd := exec.CommandContext(ctx, "bin/image-processor", "convert", "jpeg")
-	cmd := exec.CommandContext(ctx, "bin/image-processor", "scale", fmt.Sprintf("%d", transformation.Scale.GetWidth()), fmt.Sprintf("%d", transformation.Scale.GetHeight()))
+	if transformation.GetConversion() != nil {
+		// validate transformation.GetConversion().GetFormat()
+		return exec.CommandContext(ctx, "bin/image-processor", "convert", transformation.GetConversion().GetFormat()), nil
+	}
 
-	return cmd, nil
+	if transformation.GetCompression() != nil {
+		return nil, errors.New("no compression yet")
+	}
+
+	if transformation.GetCrop() != nil {
+		return nil, errors.New("no crop yet")
+	}
+
+	return nil, nil
 }
