b4a0f37f70fd9fc557c69be2ad1acc9e604222f0
lucas <241841693+lucas-woo@users.noreply.github.com>
2026-07-30 02:09:48 -0600

    initializing auth service and handler

--- internal/gateway/services/auth_service.go
+++ internal/gateway/services/auth_service.go
@@ -1,8 +1,16 @@
 package services
 
+import (
+	authv1 "github.com/lucas-woo/cloud-drive/api/auth/v1"
+)
+
 
 type AuthService struct {
-
+	authclient authv1.AuthServiceClient
 }
 
-
+func NewAuthServer(authclient authv1.AuthServiceClient) *AuthService {
+	return &AuthService{
+		authclient: authclient,
+	}
+}
