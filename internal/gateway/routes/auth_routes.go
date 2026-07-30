030114c646af8fd7fc257748945bbbad960721d3
lucas <241841693+lucas-woo@users.noreply.github.com>
2026-07-30 08:17:00 -0600

    middlewares init

--- internal/gateway/routes/auth_routes.go
+++ internal/gateway/routes/auth_routes.go
@@ -3,9 +3,10 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/lucas-woo/cloud-drive/internal/gateway/handlers"
+	"github.com/lucas-woo/cloud-drive/internal/gateway/middlewares"
 )
 
-func InitializeAuthRoutes(ginEngine *gin.Engine, authHandler *handlers.AuthHandler) {
+func InitializeAuthRoutes(ginEngine *gin.Engine, middlewares *middlewares.Middlewares ,authHandler *handlers.AuthHandler) {
 
 	ginEngine.POST("/login", authHandler.Login)
 
--- internal/gateway/routes/router.go
+++ internal/gateway/routes/router.go
@@ -3,10 +3,11 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/lucas-woo/cloud-drive/internal/gateway/handlers"
+	"github.com/lucas-woo/cloud-drive/internal/gateway/middlewares"
 )
 
 
 
-func InitializeRouter(ginEngine *gin.Engine, authHandler *handlers.AuthHandler) {
-	InitializeAuthRoutes(ginEngine, authHandler)
+func InitializeRouter(ginEngine *gin.Engine, middlewares *middlewares.Middlewares, authHandler *handlers.AuthHandler) {
+	InitializeAuthRoutes(ginEngine, middlewares, authHandler)
 }
