584fac4deb62c7fe0cea04b61f5cd9832b5c0645
lucas <241841693+lucas-woo@users.noreply.github.com>
2026-07-30 23:23:16 -0600

    signup handler, service, route

--- internal/gateway/handlers/auth_handler.go
+++ internal/gateway/handlers/auth_handler.go
@@ -1,7 +1,10 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/lucas-woo/cloud-drive/internal/config"
 	"github.com/lucas-woo/cloud-drive/internal/dto"
 	"github.com/lucas-woo/cloud-drive/internal/gateway/services"
 )
@@ -10,15 +13,30 @@ type AuthHandler struct {
 	authService *services.AuthService
 }
 
-func (h *AuthHandler) SignUp(ctx *gin.Context) {
+func (h *AuthHandler) SignUp(c *gin.Context) {
+	var signUpReq dto.GatewaySignUpRequest
+	if err := c.ShouldBindBodyWithJSON(&signUpReq); err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	sessionId, err := h.authService.SignUp(c.Request.Context(), &signUpReq)
+	if err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return		
+	}
 	
+	c.SetCookie(config.CookieSession, sessionId, config.CookieSessionMaxAge, config.CookieSessionPath, config.CookieSessionDomain, config.CookieSessionSecure, config.CookieSessionHttpOnly)
+
+	c.JSON(http.StatusCreated, "created")
+
 }
 
-func (h *AuthHandler) Login(ctx *gin.Context) {
+func (h *AuthHandler) Login(c *gin.Context) {
 }
 
 
-func (h *AuthHandler) Logout(ctx *gin.Context) {
+func (h *AuthHandler) Logout(c *gin.Context) {
 	
 }
 
--- internal/gateway/services/auth_service.go
+++ internal/gateway/services/auth_service.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	authv1 "github.com/lucas-woo/cloud-drive/api/auth/v1"
+	"github.com/lucas-woo/cloud-drive/internal/dto"
 )
 
 
@@ -11,12 +12,20 @@ type AuthService struct {
 	authclient authv1.AuthServiceClient
 }
 
-func (s *AuthService) Login(ctx context.Context) {
+func (s *AuthService) SignUp(ctx context.Context, req *dto.GatewaySignUpRequest) (string, error){
+	res, err := s.authclient.SignUpUser(ctx, &authv1.SignUpUserRequest{
+		Username: req.Username,
+		Email: req.Email,
+		Password: req.Password,
+		RememberMe: req.RememberMe,
+	})
+	return res.GetSessionId(), err
+}
+
+func (s *AuthService) Login(ctx context.Context, ) {
 	// s.authclient.LoginUser()
 }
-func (s *AuthService) SignUp(ctx context.Context) {
 
-}
 func (s *AuthService) Logout(ctx context.Context) {
 	
 }
