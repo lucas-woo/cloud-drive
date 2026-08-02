bdf67992a235fcccc7458a8d7ae603ba7ad33c27
lucas <lucaswoo583@gmail.com>
2026-08-02 03:36:21 -0600

    util object converter

--- internal/utils/dto.go
+++ internal/utils/dto.go
@@ -1,3 +1,28 @@
 package utils
 
+import (
+	mediav1 "github.com/lucas-woo/cloud-drive/api/media/v1"
+	"github.com/lucas-woo/cloud-drive/internal/dto"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
 
+
+func ConvertProjectObjectsToResponse(objects []*dto.ProjectObject) ([]*mediav1.ProjectObject) {
+	resp := make([]*mediav1.ProjectObject, 0, len(objects))
+
+	for _, obj := range objects {
+		resp = append(resp, &mediav1.ProjectObject{
+			ProjectId:    obj.ProjectId.String(),
+			CollectionId: obj.CollectionId.String(),
+			FolderId:     obj.FolderId.String(),
+			ObjectId:     obj.ObjectId.String(),
+			FileSize:     obj.FileSize,
+			Format:       obj.Format,
+			IsActive:     obj.IsActive,
+			CreatedAt:    timestamppb.New(obj.CreatedAt),
+			ModifiedAt:   timestamppb.New(obj.ModifiedAt),
+		})
+	}
+
+	return resp
+}
