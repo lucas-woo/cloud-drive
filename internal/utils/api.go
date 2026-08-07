469c679622081d5101489fb9206ae55d9190fac2
lucas <241841693+lucas-woo@users.noreply.github.com>
2026-08-07 04:05:08 -0600

    project, folder, collection mappers

--- internal/utils/api.go
+++ internal/utils/api.go
@@ -16,4 +16,101 @@ func (c *RestMapper) ConvertAssetCursor(cursor *mediav1.AssetCursor) *api.AssetC
 	}	
 }
 
-// func (c *RestMapper) ConvertProject
+func (c *RestMapper) ConvertProjectObjectSlice(objects []*mediav1.ProjectObject) []api.ProjectObject {
+    if objects == nil {
+        return nil
+    }
+
+    result := make([]api.ProjectObject, 0, len(objects))
+
+    for _, obj := range objects {
+        if obj == nil {
+            continue
+        }
+
+        result = append(result, api.ProjectObject{
+            ProjectId:    obj.GetProjectId(),
+            CollectionId: obj.GetCollectionId(),
+            FolderId:     obj.GetFolderId(),
+            ObjectId:     obj.GetObjectId(),
+            FileSize:     obj.GetFileSize(),
+            Format:       obj.GetFormat(),
+            IsActive:     obj.GetIsActive(),
+            CreatedAt:    obj.GetCreatedAt().AsTime(),
+            ModifiedAt:   obj.GetModifiedAt().AsTime(),
+        })
+    }
+
+    return result
+}
+
+func (c *RestMapper) ConvertCollectionSlice(collections []*mediav1.ProjectCollection) []api.Collection {
+	if collections == nil {
+		return nil
+	}
+
+	result := make([]api.Collection, 0, len(collections))
+
+	for _, collection := range collections {
+		if collection == nil {
+			continue
+		}
+
+		item := api.Collection{
+			CollectionId: collection.GetCollectionId(),
+			Name:         collection.GetName(),
+			Description:  collection.GetDescription(),
+			IsPublic:     collection.GetIsPublic(),
+		}
+
+		if collection.GetCreatedAt() != nil {
+			item.CreatedAt = collection.GetCreatedAt().AsTime()
+		}
+
+		if collection.GetLastModified() != nil {
+			item.LastModified = collection.GetLastModified().AsTime()
+		}
+
+		result = append(result, item)
+	}
+
+	return result
+}
+
+
+func (c *RestMapper) ConvertFolderSlice(folders []*mediav1.ProjectFolder) []api.Folder {
+	if folders == nil {
+		return nil
+	}
+
+	result := make([]api.Folder, 0, len(folders))
+
+	for _, folder := range folders {
+		if folder == nil {
+			continue
+		}
+
+		item := api.Folder{
+			FolderId:   folder.GetFolderId(),
+			FolderName: folder.GetFolderName(),
+			FolderSize: folder.GetFolderSize(),
+			AssetCount: folder.GetAssetCount(),
+		}
+
+		if folder.GetLastUplaod() != nil {
+			item.LastUpload = folder.GetLastUplaod().AsTime()
+		}
+
+		if folder.GetCreatedAt() != nil {
+			item.CreatedAt = folder.GetCreatedAt().AsTime()
+		}
+
+		if folder.GetModifiedAt() != nil {
+			item.ModifiedAt = folder.GetModifiedAt().AsTime()
+		}
+
+		result = append(result, item)
+	}
+
+	return result
+}
