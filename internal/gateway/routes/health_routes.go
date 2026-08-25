b85d6d7136a1919ce1249a8ea49b3d8ed9cd67fb
lucas <241841693+lucas-woo@users.noreply.github.com>
2026-08-25 17:16:20 -0600

    health handler

---
+++ internal/gateway/routes/health_routes.go
@@ -0,0 +1,10 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/lucas-woo/cloud-drive/internal/gateway/handlers"
+)
+
+func InitializeHealthRoutes(healthGroup *gin.RouterGroup, healthHandlers *handlers.AwsHandler) {
+
+}
