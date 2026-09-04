cdbd04bd33032673381dc31d5a83b38ce429b5d7
lucas <lucaswoo583@gmail.com>
2026-09-03 21:31:01 -0600

    metadata util

--- internal/dto/transformations.go
+++ internal/dto/transformations.go
@@ -9,3 +9,11 @@ type ScaleTransformation struct {
 	Height uint32 `json:"height"`
 	Width uint32 `json:"width"`
 }
+
+type ConvertTransformation struct {
+	Format string `json:"format"`
+}
+
+type CompressTransformation struct {
+	Compress bool `json:"compress"`
+}
