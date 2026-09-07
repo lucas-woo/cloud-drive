234591f0b7cbe464b07ae3ae579eb2d0090119bf
lucas <241841693+lucas-woo@users.noreply.github.com>
2026-09-07 02:18:41 -0600

    eventbridge confirmation service

--- internal/api/s3.go
+++ internal/api/s3.go
@@ -1,6 +1,6 @@
 package api
 
 type S3UploadEventRequest struct {
-	ObjectId     string `json:"objectId" binding:"required"`
+	ObjectPath     string `json:"objectPath" binding:"required"`
 	FileSize     uint64 `json:"fileSize" binding:"gte=0"`
 }
