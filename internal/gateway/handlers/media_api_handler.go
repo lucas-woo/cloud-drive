6850c95c8053a9c3de5b3a088f09a8c47a853013
lucas <241841693+lucas-woo@users.noreply.github.com>
2026-09-14 03:50:57 -0600

    get all folders handler

--- internal/gateway/handlers/media_api_handler.go
+++ internal/gateway/handlers/media_api_handler.go
@@ -241,6 +241,33 @@ func (h *MediaApiHandler) GetProjectId(c *gin.Context) {
 	})
 }
 
+func (h *MediaApiHandler) GetAllFolders(c *gin.Context) {
+	apiKey := c.GetHeader("API-Key")
+	apiSecret := c.GetHeader("API-Secret")
+
+	if apiKey == "" || apiSecret == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+					"error": "missing API credentials",
+			})
+			return
+	}	
+
+	ok, err := h.service.ValidateApiKey(c.Request.Context(), apiKey, apiSecret, iamv1.ValidateApiKeyPermissionRequest_PERMISSION_CREATE)
+
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return		
+	}
+
+	if !ok {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "permission denied",
+		})
+		return		
+	}	
+	
+}
+
 func NewMediaApiHandler(service *services.MediaApiService) *MediaApiHandler{
 	return &MediaApiHandler{
 		service: service,
