af57c8939423e6f4def866ecf111f9b022b2dc78
lucas <lucaswoo583@gmail.com>
2026-09-14 05:14:28 -0600

    mapper

--- internal/utils/api.go
+++ internal/utils/api.go
@@ -263,6 +263,21 @@ func (m *RestMapper) ToProtoTransformations(
 	}
 }
 
-func (m *RestMapper) ConvertAllFoldersToApiResponse(folders []*mediav1.ProjectFolder) (*api.GetAllFoldersApiResponse) {
-	return nil
+func (m *RestMapper) ConvertAllFoldersToApiResponse(folders []*mediav1.ProjectFolder) *api.GetAllFoldersApiResponse {
+	response := &api.GetAllFoldersApiResponse{
+		Folders: make([]api.FolderApi, 0, len(folders)),
+	}
+
+	for _, folder := range folders {
+		if folder == nil {
+			continue
+		}
+
+		response.Folders = append(response.Folders, api.FolderApi{
+			FolderId:   folder.FolderId,
+			FolderName: folder.FolderName,
+		})
+	}
+
+	return response
 }
